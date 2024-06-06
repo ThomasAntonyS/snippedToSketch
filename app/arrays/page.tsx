f2709d17c21b61ec3330da590e053740f31d3d82
Babithkp <babithkullachetti44@gmail.com>
2024-06-07 00:52:08 +0530

    first dummy push

---
+++ app/arrays/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ArrayFunction from "./components/ArrayFunction"
+
+export default function page() {
+
+
+  return (
+    <main className="grid min-h-screen grid-rows-[3rem,22rem,1fr] bg-slate-500">
+      <ArrayFunction/>
+    </main>
+  );
+}

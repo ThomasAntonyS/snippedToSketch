259f22ecc979ae436fdc1dc5492881aa88aba0ed
Babithkp <babithkullachetti44@gmail.com>
2024-06-08 09:09:01 +0530

    basic oparetion - layout has added

---
+++ app/stacks/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <main className="grid min-h-screen grid-rows-[3rem,22rem,1fr] bg-slate-500">
+      <div></div>
+      <section className="mx-10 flex grid-rows-2 flex-col items-center justify-center bg-white"></section>
+      <section className="bg-orange-300 p-2"></section>
+    </main>
+  )
+}

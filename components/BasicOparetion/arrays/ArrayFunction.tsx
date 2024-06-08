a19b908d8be4ae36f24e9a3b8063b87d98062f7e
Babithkp <babithkullachetti44@gmail.com>
2024-06-09 00:29:10 +0530

    array oparetion started insert

--- components/BasicOparetion/arrays/ArrayFunction.tsx
+++ components/BasicOparetion/arrays/ArrayFunction.tsx
@@ -16,6 +16,9 @@ const boxClassVariable =
   "flex h-14 w-14 items-center justify-center rounded-lg bg-rose-500 text-xl font-medium drop-shadow-lg ";
 
 export default function ArrayFunction() {
+
+  const [insertNumberIndex, setInsertNumberIndex] = useState("4");
+  const [insertNumber, setInsertNumber] = useState("24");
   const [createNumber, setCreateNumber] = useState(5);
   const [appendNumber, setAppendNumber] = useState(6);
   const [defaultText, setDefaultText] = useState(true);
@@ -34,6 +37,7 @@ export default function ArrayFunction() {
     setAppendNumber(event.currentTarget.value);
   };
 
+
   const generateDefaultArray = () => {
     const newNumbers = [];
     for (let i =0;i< createNumber;i++) {
@@ -48,16 +52,34 @@ export default function ArrayFunction() {
   };
 
   const appendOneElement = () => {
-    
     setMainArray((prev) => [...prev, {
       value: appendNumber,
       id: appendNumber.toString()+arrayLength.toString()
     }]);
     setDefaultText(false);
-    onAppendButton();
   };
 
+  const insertToIndex = () => {
+    let lastInx = arrayLength;
+    let newArray = [...mainArray]; // copy of the mainArray
+
+     while (Number(insertNumberIndex) < lastInx) {
+      newArray[lastInx] = newArray[lastInx - 1];
+      lastInx--;
+    }
+
+    newArray[lastInx] = {
+      id: insertNumber + insertNumberIndex,
+      value: Number(insertNumber),
+    };
+
+    setMainArray(newArray)
+    setDefaultText(false);
+    }
+    
+
   const onAppendButton = contextSafe(() => {
+    appendOneElement()
     setTimeout(() => {
       gsap.fromTo(`.box${appendNumber}${arrayLength}`, 
         {
@@ -113,7 +135,7 @@ export default function ArrayFunction() {
             Please Click Any Operation to view the Arrays
           </span>
         )}
-        <div className="flex">
+        <div className="flex" ref={container}>
           {mainArray.map((ele, i) => (
             <div key={i}>
               <div className="border-2 border-black p-1">
@@ -150,7 +172,7 @@ export default function ArrayFunction() {
           <div className="flex flex-col items-center gap-3">
             <Button
               className="w-[5rem] hover:scale-100"
-              onClick={appendOneElement}
+              onClick={onAppendButton}
             >
               Append
             </Button>
@@ -168,24 +190,24 @@ export default function ArrayFunction() {
           <div className="flex flex-col items-center gap-3">
             <Button
               className="w-[5rem] hover:scale-100"
-              onClick={onCreateButton}
+              onClick={insertToIndex}
             >
               Insert
             </Button>
             <span className="text-md w-[60%] font-medium">
               To Insert New Value has
               <input
-                className="w-10 p-1"
+                className="w-12 p-1"
                 type="number"
-                onChange={createInputhander}
-                value={createNumber}
+                onChange={(e)=>setInsertNumber(e.target.value)}
+                value={insertNumber}
               />{" "}
               To Index number 
               <input
                 className="w-10 p-1"
                 type="number"
-                onChange={createInputhander}
-                value={createNumber}
+                onChange={(e)=>setInsertNumberIndex(e.target.value)}
+                value={insertNumberIndex}
               />{" "}
             </span>
           </div>

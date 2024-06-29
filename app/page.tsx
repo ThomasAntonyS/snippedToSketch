adb74055a7642c11a54735a1ef295e92118425ad
Thomas Antony S <srtomsam@gmail.com>
2024-06-30 00:03:13 +0530

    developed about n started visualize section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import AboutSection from "@/components/home/Aboutsection";
 import Herosection from "@/components/home/Herosection";
+import Visualizesection from "@/components/home/Visualizesection";
 
 export default function Home() {
   return (
@@ -7,10 +9,10 @@ export default function Home() {
       <Herosection/>
 
       {/* ------------------------------- AboutUs Section  ------------------------------- */}
-      <div className="AboutUs_Section"></div>
+      <AboutSection/>
 
       {/* ------------------------------- Visualize Section  ------------------------------- */}
-      <div className="Visualize_Section"></div>
+      <Visualizesection/>
     </main>
   );
 }
